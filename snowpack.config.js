04868adda0c011c61055f9ca9929faf39e5651fa
GamingInfinite <ender2gamer123@gmail.com>
2021-10-04 00:31:44 -0500

    removing webpack and adding filepond for later

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     public: "/",
     src: "/dist",
   },
-  plugins: ["@snowpack/plugin-svelte", "@snowpack/plugin-webpack"],
+  plugins: ["@snowpack/plugin-svelte"],
   packageOptions: {
     /* ...*/
   },
